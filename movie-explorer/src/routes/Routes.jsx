deb7d2af489ff7667593cba0728688494d593d6b
Tharushika Sirimanne <160596955+dtssirimanne@users.noreply.github.com>
2025-06-04 14:28:35 +0530

    updated

---
+++ movie-explorer/src/routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route,Switch } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Catalog from "../pages/Catalog";
+import Detail from "../pages/detail/Detail";
+
+
+import * as Config from "../constants/Config";
+
+
+const Routes = () =>{
+    
+}
