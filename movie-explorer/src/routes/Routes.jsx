17b6f8587a080a6851452b4acd0f37a69a9c9f70
Tharushika Sirimanne <160596955+dtssirimanne@users.noreply.github.com>
2025-06-04 15:06:32 +0530

    Added

--- movie-explorer/src/routes/Routes.jsx
+++ movie-explorer/src/routes/Routes.jsx
@@ -10,5 +10,16 @@ import * as Config from "../constants/Config";
 
 
 const Routes = () =>{
-    
-}
+    return(
+        <Switch>
+            <Route 
+                path={`/${Config.HOME_PAGE}/:category/search/:keyword`}
+                component={Catalog}
+            />
+            <Route path={`/${Config.HOME_PAGE}/:category/id`}component={Detail}/>
+             <Route path={`/${Config.HOME_PAGE}/:category`}component={Catalog}/>
+            <Route path={`/${Config.HOME_PAGE}`} exact component={Home}/>
+
+        </Switch>
+    );
+};
